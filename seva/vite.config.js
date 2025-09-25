68c1e2cf5f83f36960da87723769e6e7ede68145
JohanR7 <143104864+JohanR7@users.noreply.github.com>
2025-09-26 01:33:27 +0530

    vite config for deployment

--- seva/vite.config.js
+++ seva/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    server: {
-    host: true}
+  plugins: [react()]
 })
